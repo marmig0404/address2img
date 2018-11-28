1c9639b7791664c8a49dcd3d2b0a2f348bff29b6
Martin Miglio <marmig0404@gmail.com>
2018-11-28 18:53:54 -0500

    updated example.py

--- example.py
+++ example.py
@@ -1,6 +1,9 @@
 # importing map_maker file from address2img
 from address2img import map_maker
 
+# defining config file
+config_file = 'config.ini'
+
 # defining addresses for which render maps, must be a list
 addresses = [
     'Piazza del Duomo, 56126 Pisa PI, Italy', # Leaning Tower of Pisa
@@ -8,8 +11,8 @@ addresses = [
     'Parque Nacional da Tijuca, Alto da Boa Vista, Rio de Janeiro, Brazil',
 ]
 
-# instantiating Map_Maker class with addresses
-worker = map_maker.Map_Maker(addresses)
+# instantiating Map_Maker class with addresses and config file
+worker = map_maker.Map_Maker(addresses, config_file)
 
 # calling make_map method of the Map_Maker instance
 worker.make_map()
