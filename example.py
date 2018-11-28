a67cc08e5ced513751c0bd5bc4eaea0e9407fa99
Martin Miglio <marmig0404@gmail.com>
2018-11-28 18:08:58 -0500

    Add example code

---
+++ example.py
@@ -0,0 +1,15 @@
+# importing map_maker file from address2img
+from address2img import map_maker
+
+# defining addresses for which render maps, must be a list
+addresses = [
+    'Piazza del Duomo, 56126 Pisa PI, Italy', # Leaning Tower of Pisa
+    '1600 Pennsylvania Ave NW, Washington, DC 20500', # The White House
+    'Parque Nacional da Tijuca, Alto da Boa Vista, Rio de Janeiro, Brazil',
+]
+
+# instantiating Map_Maker class with addresses
+worker = map_maker.Map_Maker(addresses)
+
+# calling make_map method of the Map_Maker instance
+worker.make_map()
